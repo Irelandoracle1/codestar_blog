2c7f840574b41c28a1d34c3b7db81bf24b73f0b4
Irelandoracle1 <100025415+Irelandoracle1@users.noreply.github.com>
2025-08-16 22:56:41 +0100

    setting up for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9p)g(3mfgnl+i$uk#=v=3es1xjy_%b)@dk2=-2n+65o97jr^3)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
